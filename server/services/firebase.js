142f56274fc23276e4b5371a14b733be5defb440
Anarv Vasavada <138287585+Anarv2104@users.noreply.github.com>
2025-03-10 11:45:49 +0530

    Update firebase.js

--- server/services/firebase.js
+++ server/services/firebase.js
@@ -3,12 +3,12 @@ const { getFirestore } = require("firebase/firestore");
 require("dotenv").config(); // Load environment variables
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDcskHkK0vW5qk5S5ZNmOC4BQQTFt-U8fc",
-    authDomain: "visionxapp-9347e.firebaseapp.com",
-    projectId: "visionxapp-9347e",
-    storageBucket: "visionxapp-9347e.firebasestorage.app",
-    messagingSenderId: "333547970756",
-    appId: "1:333547970756:web:0171347023c8e7bbaca991"
+    apiKey: "Your_firebase_Api_key",
+    authDomain: "firebase_app_domain",
+    projectId: "firebase_project_id",
+    storageBucket: "firebase_storageBucket",
+    messagingSenderId: "messageSenderId",
+    appId: "firebase_appid"
 };
 
 // Initialize Firebase (backend)
